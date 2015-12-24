ff68269e42c9b20feec85304fd13b25b2cdcf288
kaestel <martin@think.dk>
2015-12-24 12:45:05 +0100

    Updated mail texts

--- src/templates/pages/signup_confirmed.php
+++ src/templates/pages/signup_confirmed.php
@@ -12,15 +12,11 @@ $username = $action[2];
 	<p>Din <?= $type ?>: <?= $username ?>, er hermed bekræftet.</p>
 
 	<p>
-		Via <a href="/janitor/admin/profile">Janitor</a> kan du opdatere din profil og annullere din nysgerrighed,
-		hvis det skulle blive nødvendigt.
+		Via <a href="/janitor/admin/profile">Janitor</a> kan du opdatere din profil eller annullere din nysgerrighed,
+		hvis det skulle blive nødvendigt. Dit brugernavn er din email og hvis du ikke selv har valgt et password, står 
+		dét i den tilsendte email.
 	</p>
 
 	<p>Tak for din nysgerrighed - du hører fra os!</p>
-	<!--p>
-		Selvom du nu kan kommentere på artiklerne, vil vi godt indrømme
-		at Stopknappen udøver censur her på sitet. Hvis du bidrager positivt er du mere end velkommen,
-		men hvis ikke, vil vi venligt henstille til at du finder et andet forum.
-	</p-->
 
 </div>
