929a3ebb6878af96eb684caf93aa06ff033a2a31
threepwoodd <scope.dk@gmail.com>
2018-11-22 16:29:19 +0100

    Update js include paths

--- theme/www/js/lib/seg_smartphone_include.js
+++ theme/www/js/lib/seg_smartphone_include.js
@@ -1,43 +1,27 @@
-document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/v0_9_1/merged/seg_smartphone.js"></script>');
+document.write('<script type="text/javascript" src="assets/parentnode-skin-default/js/seg_smartphone.js"></script>');
 
-document.write('<script type="text/javascript" src="/js/lib/u-settings.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/u-basics.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/u-googleanalytics.js"></script>');
+document.write('<script type="text/javascript" src="assets/parentnode-skin-default/js/manipulator/src/u-googleanalytics.js"></script>');
 
-document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/beta-u-eventchain.js"></script>');
+document.write('<script type="text/javascript" src="assets/parentnode-skin-default/js/manipulator/src/beta-u-fontsReady.js"></script>');
 
-document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/beta-u-animation-to.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/beta-u-fontsReady.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/beta-u-notifier.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/beta-u-paymentcards.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/beta-u-googlemaps.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/u-textscaler.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/u-svg.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/u-date.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/u-form.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/u-string.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/u-form-builder.js"></script>');
-
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/smartphone/u-form-custom.js"></script>');
-document.write('<script type="text/javascript" src="/js/manipulator/src/beta-u-timeline.js"></script>');
+document.write('<script type="text/javascript" src="assets/parentnode-skin-default/js/manipulator/src/beta-u-eventchain.js"></script>');
+document.write('<script type="text/javascript" src="assets/parentnode-skin-default/js/manipulator/src/beta-u-timeline.js"></script>');
+document.write('<script type="text/javascript" src="assets/parentnode-skin-default/js/manipulator/src/beta-u-paymentcards.js"></script>');
+document.write('<script type="text/javascript" src="assets/parentnode-skin-default/js/manipulator/src/beta-u-googlemaps.js"></script>');
 
 
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/smartphone/i-page.js"></script>');
+document.write('<script type="text/javascript" src="js/lib/smartphone/i-front.js"></script>');
+document.write('<script type="text/javascript" src="js/lib/smartphone/i-signup.js"></script>');
+document.write('<script type="text/javascript" src="js/lib/smartphone/i-buttons.js"></script>');
+document.write('<script type="text/javascript" src="js/lib/smartphone/i-stop.js"></script>');
+document.write('<script type="text/javascript" src="js/lib/smartphone/i-start.js"></script>');
+document.write('<script type="text/javascript" src="js/lib/smartphone/i-topic.js"></script>');
+
+document.write('<script type="text/javascript" src="js/lib/u-settings.js"></script>');
+document.write('<script type="text/javascript" src="js/lib/desktop/u-basics.js"></script>');
+
 document.write('<script type="text/javascript" src="http://think.local/js/lib/smartphone/i-scene.js"></script>');
 document.write('<script type="text/javascript" src="http://think.local/js/lib/smartphone/i-login.js"></script>');
 document.write('<script type="text/javascript" src="http://think.local/js/lib/u-basics.js"></script>');
-
-
-document.write('<script type="text/javascript" src="/js/lib/smartphone/i-front.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/smartphone/i-signup.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/smartphone/i-buttons.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/smartphone/i-stop.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/smartphone/i-start.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/smartphone/i-topic.js"></script>');
-
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/desktop/i-article.js"></script>');
-
-document.write('<script type="text/javascript" src="/js/lib/desktop/u-basics.js"></script>');
-
 
 
--- theme/www/js/lib/seg_tablet_include.js
+++ theme/www/js/lib/seg_tablet_include.js
@@ -1,40 +1,33 @@
-document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/v0_9_1/merged/seg_tablet.js"></script>');
+// Manipulator bundle
+document.write('<script type="text/javascript" src="assets/parentnode-skin-default/js/seg_tablet.js"></script>');
 
-document.write('<script type="text/javascript" src="/js/lib/u-settings.js"></script>');
-document.write('<script type="text/javascript" src="/js/manipulator/src/u-googleanalytics.js"></script>');
+// Additional includes from parentnode-skin-default
+document.write('<script type="text/javascript" src="assets/parentnode-skin-default/js/manipulator/src/u-googleanalytics.js"></script>');
 
-document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/beta-u-animation-to.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/beta-u-fontsReady.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/beta-u-notifier.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/u-textscaler.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/u-svg.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/u-date.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/u-scrollto.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/manipulator/src/u-string.js"></script>');
-document.write('<script type="text/javascript" src="/js/manipulator/src/beta-u-timeline.js"></script>');
+document.write('<script type="text/javascript" src="assets/parentnode-skin-default/js/manipulator/src/beta-u-timeline.js"></script>');
+
+document.write('<script type="text/javascript" src="assets/parentnode-skin-default/js/lib/desktop/i-comments.js"></script>');
+document.write('<script type="text/javascript" src="assets/parentnode-skin-default/js/lib/desktop/u-sharing.js"></script>');
+document.write('<script type="text/javascript" src="assets/parentnode-skin-default/js/lib/desktop/u-checkmark.js"></script>');
+document.write('<script type="text/javascript" src="assets/parentnode-skin-default/js/lib/desktop/u-expandarrow.js"></script>');
+document.write('<script type="text/javascript" src="assets/parentnode-skin-default/js/lib/desktop/i-qnas.js"></script>');
+document.write('<script type="text/javascript" src="assets/parentnode-skin-default/js/lib/desktop/i-pagination.js"></script>');
+document.write('<script type="text/javascript" src="assets/parentnode-skin-default/js/lib/desktop/i-article_mini_list.js"></script>');
+
+// Project specific
+document.write('<script type="text/javascript" src="js/lib/u-settings.js"></script>');
+document.write('<script type="text/javascript" src="js/lib/desktop/u-basics.js"></script>');
+
+document.write('<script type="text/javascript" src="js/lib/desktop/i-front.js"></script>');
+document.write('<script type="text/javascript" src="js/lib/desktop/i-buttons.js"></script>');
+document.write('<script type="text/javascript" src="js/lib/desktop/i-signup.js"></script>');
+document.write('<script type="text/javascript" src="js/lib/desktop/i-stop.js"></script>');
+document.write('<script type="text/javascript" src="js/lib/desktop/i-start.js"></script>');
+document.write('<script type="text/javascript" src="js/lib/desktop/i-topic.js"></script>');
 
 
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/desktop/i-page.js"></script>');
+// Getting necessary u.showScene initializers from think
 document.write('<script type="text/javascript" src="http://think.local/js/lib/desktop/i-scene.js"></script>');
 document.write('<script type="text/javascript" src="http://think.local/js/lib/desktop/i-login.js"></script>');
 document.write('<script type="text/javascript" src="http://think.local/js/lib/u-basics.js"></script>');
 
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/desktop/i-comments.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/desktop/u-sharing.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/desktop/u-checkmark.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/desktop/u-expandarrow.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/desktop/i-qnas.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/desktop/i-pagination.js"></script>');
-
-
-
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-front.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-buttons.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-signup.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-stop.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-start.js"></script>');
-document.write('<script type="text/javascript" src="/js/lib/desktop/i-topic.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/desktop/i-article.js"></script>');
-document.write('<script type="text/javascript" src="http://parentnode.local/js/global/desktop/i-article_mini_list.js"></script>');
-
-document.write('<script type="text/javascript" src="/js/lib/desktop/u-basics.js"></script>');
