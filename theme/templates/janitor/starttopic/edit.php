cca5d50bab03cc205373e7ccccd7bf961d6b648a
Martin Kæstel Nielsen <martin@think.dk>
2019-10-13 19:51:25 +0200

    Prepared for janitor update

--- theme/templates/janitor/starttopic/edit.php
+++ theme/templates/janitor/starttopic/edit.php
@@ -21,7 +21,7 @@ $item = $IC->getItem(array("id" => $item_id, "extend" => array("tags" => true, "
 				<?= $model->input("name", array("value" => $item["name"])) ?>
 				<?= $model->input("description", array("class" => "autoexpand short", "value" => $item["description"])) ?>
 				<?= $model->input("fixed_url_identifier", array("value" => $item["fixed_url_identifier"])) ?>
-				<?= $model->inputHTML("html", array("value" => $item["html"])) ?>
+				<?= $model->input("html", array("value" => $item["html"])) ?>
 			</fieldset>
 
 			<?= $JML->editActions($item) ?>
